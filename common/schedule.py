3319b411471efe5599ea6affc7cb0cee975014a0
igoryup <yurakov99@gmail.com>
2024-09-26 22:15:29 +0300

    added schedule

---
+++ common/schedule.py
@@ -0,0 +1,64 @@
+from datetime import datetime, timedelta, timezone
+
+from string import Template
+
+msk_offset = timedelta(hours=3)
+msk_tz = timezone(msk_offset)
+
+class DeltaTemplate(Template):
+    delimiter = "%"
+
+def strfdelta(tdelta, fmt):
+    d = {"D": tdelta.days}
+    hours, rem = divmod(tdelta.seconds, 3600)
+    minutes, seconds = divmod(rem, 60)
+    d["H"] = '{:02d}'.format(hours)
+    d["M"] = '{:02d}'.format(minutes)
+    d["S"] = '{:02d}'.format(seconds)
+    t = DeltaTemplate(fmt)
+    return t.substitute(**d)
+
+def getCurTime():
+    return datetime.now(msk_tz)
+
+def getCurTimeFormat():
+    return datetime.now(msk_tz).strftime("%H:%M:%S")
+
+class WeeklySchedule:
+    def __init__(self):
+        # Инициализируем расписание текущим временем для всех дней недели
+        current_time = getCurTime()
+        self.schedule = {
+            "Monday": (current_time, current_time),
+            "Tuesday": (current_time, current_time),
+            "Wednesday": (current_time, current_time),
+            "Thursday": (current_time, current_time),
+            "Friday": (current_time, current_time),
+            "Saturday": (current_time, current_time),
+            "Sunday": (current_time, current_time)
+        }
+
+    def get_today(self):
+        # Получаем текущий день недели на английском
+        return datetime.datetime.today(msk_tz).strftime('%A')
+
+    def get_schedule_for_day(self, day):
+        # Получаем расписание для конкретного дня недели
+        return self.schedule.get(day, ("No data", "No data"))
+
+    def get_today_schedule(self):
+        # Получаем расписание для текущего дня недели
+        today = self.get_today()
+        return today, self.get_schedule_for_day(today)
+
+    def set_start_time_for_today(self, start_time):
+        # Устанавливаем время начала работы для текущего дня
+        today = self.get_today()
+        end_time = self.schedule[today][1]  # Сохраняем текущее время окончания
+        self.schedule[today] = (start_time, end_time)
+
+    def set_end_time_for_today(self, end_time):
+        # Устанавливаем время окончания работы для текущего дня
+        today = self.get_today()
+        start_time = self.schedule[today][0]  # Сохраняем текущее время начала
+        self.schedule[today] = (start_time, end_time)
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -17,6 +17,7 @@ from datetime import datetime, timedelta, timezone
 
 from string import Template
 
+import common.schedule
 
 msk_offset = timedelta(hours=3)
 msk_tz = timezone(msk_offset)
@@ -34,22 +35,24 @@ def strfdelta(tdelta, fmt):
     t = DeltaTemplate(fmt)
     return t.substitute(**d)
 
-start_times = {}
+users_schedule = {}
 
 user_private_router = Router()
 user_private_router.message.filter(ChatTypeFilter(["private"]))
 
 USER_KB = get_keyboard(
     "Начать день",
+    "Отчет за неделю",
     placeholder="Выберите действие",
-    sizes=(1, 0)
+    sizes=(1, 1)
 )
 
 USER_EXT_KB = get_keyboard(
     "Проверить время",
     "Закончить день",
+    "Отчет за неделю",
     placeholder="Выберите действие",
-    sizes=(1, 1)
+    sizes=(1, 2)
 )
 
 @user_private_router.message(CommandStart())
@@ -58,48 +61,73 @@ async def start_cmd(message: types.Message):
         "Привет, я виртуальный помощник",
         reply_markup=USER_KB
     )
+    user_id = message.from_user.id 
+    users_schedule[user_id] = common.WeeklySchedule()
 
 @user_private_router.message(or_f(Command("Начать день"), (F.text.lower() == "начать день")))
 async def begin_cmd(message: types.Message):
     user_id = message.from_user.id    
-    if user_id in start_times:
-        await message.reply(f"Вы уже отметились!")
-    else:
-        start_times[user_id] = datetime.now(msk_tz)
-        work_time = (start_times[user_id]).strftime("%H:%M:%S")
-        await message.reply(f"Время начала работы зафиксировано:\n\n<b>{work_time}</b>", reply_markup=USER_EXT_KB)
-
-        # Задержка на 8 часов
-        delay_hours = 8
-        delay_seconds = delay_hours * 3600
-        await asyncio.sleep(delay_seconds)
-        if user_id in start_times:
-            await message.reply(f"Время прошло, пора бы домой!", reply_markup=USER_EXT_KB)
+    sched = users_schedule[user_id]
+    timestamp = common.getCurTime()
+    sched.set_start_time_for_today(timestamp)
+    work_time = timestamp.strftime("%H:%M:%S")
+    await message.reply(f"Время начала работы зафиксировано:\n\n<b>{work_time}</b>", reply_markup=USER_EXT_KB)
+    # Задержка на 8 часов
+    delay_hours = 8
+    delay_seconds = delay_hours * 3600
+    await asyncio.sleep(delay_seconds)
+    await message.reply(f"Время прошло, пора бы домой!", reply_markup=USER_EXT_KB)
 
         
 @user_private_router.message(or_f(Command("Проверить время"), (F.text.lower() == "проверить время")))
 async def check_cmd(message: types.Message):
     user_id = message.from_user.id    
-    if user_id in start_times:
-        start_time = start_times[user_id]
-        end_time = datetime.now(msk_tz)
-        duration = end_time - start_time
-        work_time = strfdelta(duration, '%H:%M:%S')
-        # await message.reply("Вы закончили работать.")
-        await message.reply(f"На текущий момент Вы проработали:\n\n<b>{work_time}</b>.")
-    else:
-        await message.reply("Вы еще не отметились!")
+    sched = users_schedule[user_id]
+    start_time = sched.get_today_schedule()[0]
+    end_time = common.getCurTime()
+    duration = end_time - start_time
+    work_time = common.strfdelta(duration, '%H:%M:%S')
+    await message.reply(f"На текущий момент Вы проработали:\n\n<b>{work_time}</b>.")
 
 @user_private_router.message(or_f(Command("Закончить день"), (F.text.lower() == "закончить день")))
 async def end_cmd(message: types.Message):
     user_id = message.from_user.id
-    if user_id in start_times:
-        start_time = start_times.pop(user_id)
-        end_time = datetime.now(msk_tz)
-        duration = end_time - start_time
-        end_time = end_time.strftime("%H:%M:%S")
-        work_time = strfdelta(duration, '%H:%M:%S')
-        # await message.reply("Вы закончили работать.")
-        await message.reply(f"Рабочее время завершено.\n\nВремя окончания работы зафиксировано:\n\n<b>{end_time}</b>.\n\nВы проработали:\n\n<b>{work_time}</b>.", reply_markup=USER_KB)
-    else:
-        await message.reply("Время начала работы не было зафиксировано.")
+    sched = users_schedule[user_id]
+    start_time = sched.get_today_schedule()[0]
+    end_time = common.getCurTime()
+    duration = end_time - start_time
+    sched.set_start_time_for_today(end_time)
+    work_time = strfdelta(duration, '%H:%M:%S')
+    # await message.reply("Вы закончили работать.")
+    await message.reply(f"Рабочее время завершено.\n\nВремя окончания работы зафиксировано:\n\n<b>{end_time}</b>.\n\nВы проработали:\n\n<b>{work_time}</b>.", reply_markup=USER_KB)
+
+@user_private_router.message(or_f(Command("Отчет за неделю"), (F.text.lower() == "отчет за неделю")))
+async def check_cmd(message: types.Message):
+    user_id = message.from_user.id
+    sched = users_schedule[user_id]
+    mon = sched.get_schedule_for_day("Monday")
+    tue = sched.get_schedule_for_day("Tuesday")
+    wed = sched.get_schedule_for_day("Wednesday")
+    thu = sched.get_schedule_for_day("Thursday")
+    fri = sched.get_schedule_for_day("Friday")
+    sat = sched.get_schedule_for_day("Saturday")
+    sun = sched.get_schedule_for_day("Sunday")
+    mon_dur = mon[1] - mon[0]
+    tue_dur = tue[1] - tue[0]
+    wed_dur = wed[1] - wed[0]
+    thu_dur = thu[1] - thu[0]
+    fri_dur = fri[1] - fri[0]
+    sat_dur = sat[1] - sat[0]
+    sun_dur = sun[1] - sun[0]
+    total = mon_dur + tue_dur + wed_dur + thu_dur + fri_dur + sat_dur + sun_dur
+    total = strfdelta(total, '%H:%M:%S')
+    text = f"За текущую неделю вы отработали {total}."
+    mon_time = f"Понедельник: {strfdelta(mon[1] - mon[0], '%H:%M:&S')}"
+    tue_time = f"Вторник: {strfdelta(tue[1] - tue[0], '%H:%M:&S')}"
+    wed_time = f"Среда: {strfdelta(wed[1] - wed[0], '%H:%M:&S')}"
+    thu_time = f"Четверг: {strfdelta(thu[1] - thu[0], '%H:%M:&S')}"
+    fri_time = f"Пятница: {strfdelta(fri[1] - fri[0], '%H:%M:&S')}"
+    sat_time = f"Суббота: {strfdelta(sat[1] - sat[0], '%H:%M:&S')}"
+    sun_time = f"Воскресенье: {strfdelta(sun[1] - sun[0], '%H:%M:&S')}"
+    text += mon_time + '\n' + tue_time + '\n' + wed_time + '\n' + thu_time + '\n' + fri_time + '\n' + sat_time + '\n' + sun_time
+    await message.reply(text)
