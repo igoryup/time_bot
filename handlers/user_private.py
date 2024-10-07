7f1ec98a20355f1bea147e3b3497d9491b2b904b
igoryup <yurakov99@gmail.com>
2024-10-07 21:28:33 +0300

    Update user_private.py

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -68,13 +68,10 @@ USER_EXT_KB = get_keyboard(
 @user_private_router.message(CommandStart())
 async def start_cmd(message: types.Message):
     user_id = message.from_user.id 
-    await message.reply(f"sch = common.schedule.WeeklySchedule()")
     sch = common.schedule.WeeklySchedule()
-    await message.reply(f"sch.update_week()")
     sch.update_week()
-    await message.reply(f"key = {user_id, common.schedule.getCurWeek()}")
-    key = {user_id, common.schedule.getCurWeek()}
-    await message.reply(f"users_schedule[key] = sch")
+    # key = {user_id, common.schedule.getCurWeek()}
+    key = str(user_id) + str(common.schedule.getCurWeek())
     users_schedule[key] = sch
     await message.answer(
         "Привет, я виртуальный помощник",
@@ -84,7 +81,8 @@ async def start_cmd(message: types.Message):
 @user_private_router.message(or_f(Command("Начать день"), (F.text.lower() == "начать день")))
 async def begin_cmd(message: types.Message):
     user_id = message.from_user.id   
-    key = {user_id, common.schedule.getCurWeek()}
+    # key = {user_id, common.schedule.getCurWeek()}
+    key = str(user_id) + str(common.schedule.getCurWeek())
     sched = users_schedule[key]
     timestamp = common.schedule.getCurTime()
     sched.set_start_time_for_today(timestamp)
@@ -100,7 +98,8 @@ async def begin_cmd(message: types.Message):
 @user_private_router.message(or_f(Command("Проверить время"), (F.text.lower() == "проверить время")))
 async def check_cmd(message: types.Message):
     user_id = message.from_user.id    
-    key = {user_id, common.schedule.getCurWeek()}
+    # key = {user_id, common.schedule.getCurWeek()}
+    key = str(user_id) + str(common.schedule.getCurWeek())
     sched = users_schedule[key]
     start_time = sched.get_today_schedule()[0]
     end_time = common.schedule.getCurTime()
@@ -111,21 +110,23 @@ async def check_cmd(message: types.Message):
 @user_private_router.message(or_f(Command("Закончить день"), (F.text.lower() == "закончить день")))
 async def end_cmd(message: types.Message):
     user_id = message.from_user.id
-    key = {user_id, common.schedule.getCurWeek()}
+    # key = {user_id, common.schedule.getCurWeek()}
+    key = str(user_id) + str(common.schedule.getCurWeek())
     sched = users_schedule[key]
     start_time = sched.get_today_schedule()[0]
     end_time = common.schedule.getCurTime()
     duration = end_time - start_time
     sched.set_end_time_for_today(end_time)
     work_time = strfdelta(duration, '%H:%M:%S')
-    end_time = strfdelta(end_time, '%H:%M:%S')
+    end_time = end_time.strftime("%H:%M:%S")
     # await message.reply("Вы закончили работать.")
     await message.reply(f"Рабочее время завершено.\n\nВремя окончания работы зафиксировано:\n\n<b>{end_time}</b>.\n\nВы проработали:\n\n<b>{work_time}</b>.", reply_markup=USER_KB)
 
 @user_private_router.message(or_f(Command("Отчет за неделю"), (F.text.lower() == "отчет за неделю")))
 async def check_cmd(message: types.Message):
     user_id = message.from_user.id
-    key = {user_id, common.schedule.getCurWeek()}
+    # key = {user_id, common.schedule.getCurWeek()}
+    key = str(user_id) + str(common.schedule.getCurWeek())
     sched = users_schedule[key]
     mon = sched.get_schedule_for_day("Monday")
     tue = sched.get_schedule_for_day("Tuesday")
@@ -134,22 +135,22 @@ async def check_cmd(message: types.Message):
     fri = sched.get_schedule_for_day("Friday")
     sat = sched.get_schedule_for_day("Saturday")
     sun = sched.get_schedule_for_day("Sunday")
-    mon_dur = mon[1] - mon[0]
-    tue_dur = tue[1] - tue[0]
-    wed_dur = wed[1] - wed[0]
-    thu_dur = thu[1] - thu[0]
-    fri_dur = fri[1] - fri[0]
-    sat_dur = sat[1] - sat[0]
-    sun_dur = sun[1] - sun[0]
+    mon_dur = mon[1] - mon[0] if mon[1] - mon[0] > timedelta(0) else timedelta(0)
+    tue_dur = tue[1] - tue[0] if tue[1] - tue[0] > timedelta(0) else timedelta(0)
+    wed_dur = wed[1] - wed[0] if wed[1] - wed[0] > timedelta(0) else timedelta(0)
+    thu_dur = thu[1] - thu[0] if thu[1] - thu[0] > timedelta(0) else timedelta(0)
+    fri_dur = fri[1] - fri[0] if fri[1] - fri[0] > timedelta(0) else timedelta(0)
+    sat_dur = sat[1] - sat[0] if sat[1] - sat[0] > timedelta(0) else timedelta(0)
+    sun_dur = sun[1] - sun[0] if sun[1] - sun[0] > timedelta(0) else timedelta(0)
     total = mon_dur + tue_dur + wed_dur + thu_dur + fri_dur + sat_dur + sun_dur
     total = strfdelta(total, '%H:%M:%S')
     text = f"За текущую неделю вы отработали {total}."
-    mon_time = f"Понедельник: {strfdelta(mon[1] - mon[0], '%H:%M:%S')}"
-    tue_time = f"Вторник: {strfdelta(tue[1] - tue[0], '%H:%M:%S')}"
-    wed_time = f"Среда: {strfdelta(wed[1] - wed[0], '%H:%M:%S')}"
-    thu_time = f"Четверг: {strfdelta(thu[1] - thu[0], '%H:%M:%S')}"
-    fri_time = f"Пятница: {strfdelta(fri[1] - fri[0], '%H:%M:%S')}"
-    sat_time = f"Суббота: {strfdelta(sat[1] - sat[0], '%H:%M:%S')}"
-    sun_time = f"Воскресенье: {strfdelta(sun[1] - sun[0], '%H:%M:%S')}"
+    mon_time = f"Понедельник: {strfdelta(mon_dur, '%H:%M:%S')}"
+    tue_time = f"Вторник: {strfdelta(tue_dur, '%H:%M:%S')}"
+    wed_time = f"Среда: {strfdelta(wed_dur, '%H:%M:%S')}"
+    thu_time = f"Четверг: {strfdelta(thu_dur, '%H:%M:%S')}"
+    fri_time = f"Пятница: {strfdelta(fri_dur, '%H:%M:%S')}"
+    sat_time = f"Суббота: {strfdelta(sat_dur, '%H:%M:%S')}"
+    sun_time = f"Воскресенье: {strfdelta(sun_dur, '%H:%M:%S')}"
     text += '\n' + '\n' + mon_time + '\n' + tue_time + '\n' + wed_time + '\n' + thu_time + '\n' + fri_time + '\n' + sat_time + '\n' + sun_time
     await message.reply(text)
